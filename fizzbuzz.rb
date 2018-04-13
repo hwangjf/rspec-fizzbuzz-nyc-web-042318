561e050954e1daecdcb33b26db034ec0836869b7
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-04-13 21:14:37 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
 def fizzbuzz (num)
-  
+  if num % 3 == 0 && num % 5 == 0
+    return "Fizzbuzz"
+  elsif num % 3 == 0
+    return "Fizz"
+  elsif num % 5 == 0
+    return "Buzz"
+  else 
+    return nil
+  end
+end
